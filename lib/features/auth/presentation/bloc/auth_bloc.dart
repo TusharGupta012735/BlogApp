c3009eaa69c0d8ab94e27637ca087de12d781d36
Tushar Gupta <kamalgupta11341@gmail.com>
2025-06-25 21:52:52 +0530

    implemented the bloc code and wrapped main app inside bloc provider

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -5,9 +6,23 @@ part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
+  final UserSignUp _userSignUp;
+  AuthBloc({required UserSignUp userSignUp})
+    : _userSignUp = userSignUp,
+      super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
+      final res = await _userSignUp(
+        UserSignupParams(
+          email: event.email,
+          name: event.name,
+          password: event.password,
+        ),
+      );
+      res.fold((l){
+        emit(AuthFailure(l.message));
+      }, (r){
+        emit(AuthSuccess(r));
+      })
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:blog_app/features/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/core/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); //as a precautinary measure do this if u do anything before the actual main function
-  await Supabase.initialize(
+  final supabase = await Supabase.initialize(
     anonKey: AppSecrets.anonKey,
     url: AppSecrets.supabaseUrl,
   );
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create:
+              (_) => AuthBloc(
+                userSignUp: UserSignUp(
+                  AuthRepositoryImpl(AuthRemoteDataSourceimpl(supabase.client)),
+                ),
+              ),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
