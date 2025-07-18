ad2bed7542308d35e32493552ec6994d66bf54a2
Tushar Gupta <kamalgupta11341@gmail.com>
2025-07-18 23:00:47 +0530

    updated initdependencies

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -7,12 +7,18 @@ import 'package:blog_app/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_in.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
+import 'package:blog_app/features/blog/data/repositories/blog_repository_impl.dart';
+import 'package:blog_app/features/blog/domain/repositories/blog_repository.dart';
+import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final serviceLocator = GetIt.instance;
 
 Future<void> initDependencies() async {
+  _initBlog();
   _initAuth();
   final supabase = await Supabase.initialize(
     anonKey: AppSecrets.anonKey,
@@ -45,4 +51,17 @@ void _initAuth() {
       appUserCubit: serviceLocator(),
     ),
   );
+}
+
+void _initBlog() {
+  serviceLocator
+    ..registerFactory<BlogRemoteDataSource>(
+      () => BlogRemoteDataSourceImpl(supabaseClient: serviceLocator()),
+    )
+    ..registerFactory<BlogRepository>(
+      () => BlogRepositoryImpl(blogRemoteDataSource: serviceLocator()),
+    )
+    ..registerFactory(() => UploadBlog(blogRepository: serviceLocator()))
+    //bloc is initialized with lazysingleton because we want to maintain the state of our bloc
+    ..registerLazySingleton(() => BlogBloc(serviceLocator()));
 }
