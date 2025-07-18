45bfda979ecf4aa6793ca2df09e57c302056e1e3
Tushar Gupta <kamalgupta11341@gmail.com>
2025-07-18 22:36:20 +0530

    implemented repository for uploading blog

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -6,7 +6,6 @@ import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:blog_app/core/common/entities/user.dart' as myUser;
-import 'package:supabase_flutter/supabase_flutter.dart';
 part 'auth_event.dart';
 part 'auth_state.dart';
 
