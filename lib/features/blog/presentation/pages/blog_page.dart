168e41fe4c247e0f4928ce561beee78e58decadc
Tushar Gupta <kamalgupta11341@gmail.com>
2025-07-19 14:13:59 +0530

    updated frontend to check if everything works fine

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,11 +1,25 @@
+import 'package:blog_app/core/utils/show_snackbar.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:blog_app/features/blog/presentation/pages/add_new_blog_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends StatefulWidget {
   static route() => MaterialPageRoute(builder: (context) => const BlogPage());
 
   const BlogPage({super.key});
+
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<BlogBloc>().add(BlogFetchAllBlog());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +34,28 @@ class BlogPage extends StatelessWidget {
               icon: Icon(CupertinoIcons.add_circled),
             ),
           ],
+        ),
+        body: BlocConsumer<BlogBloc, BlogState>(
+          listener: (context, state) {
+            if (state is BlogFailure) {
+              showSnackBar(context, state.error);
+            }
+          },
+          builder: (context, state) {
+            if (state is BlogLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (state is BlogDisplaySuccess) {
+              final blogList = state.blogs;
+              return ListView.builder(
+                itemCount: blogList.length,
+                itemBuilder: (context, index) {
+                  return Text(blogList[index].title);
+                },
+              );
+            }
+            return const SizedBox();
+          },
         ),
       ),
     );
